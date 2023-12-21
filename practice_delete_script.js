d807df26d3bdf1a3559df3d01f53da89dcba801d
Biltu <deyjayendra@gmail.com>
2023-12-21 19:51:14 +0530

    hello

---
+++ practice_delete_script.js
@@ -0,0 +1,14 @@
+'use strict'; //treat all js code as newer version 
+const accuntId = 144522;
+let accunEmail = 'deyjayendra@gmail.com';
+var accuntCity = 'Kolkata';
+let id = 'hello@123';
+
+null // it is a standalone value;
+
+//symbol => unique;
+
+console.table([accuntId,accunEmail,accuntCity,id]);
+
+//null is object
+
